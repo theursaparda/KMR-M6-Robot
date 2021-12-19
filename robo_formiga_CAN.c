c96f1d93d86648925a6c4a2005ce1080e8032f74
rodrigot735 <84532678+rodrigot735@users.noreply.github.com>
2021-12-19 12:13:21 -0300

    Update robo_formiga_CAN.c

--- robo_formiga_CAN.c
+++ robo_formiga_CAN.c
@@ -10,16 +10,22 @@
 #include <sys/ioctl.h>
 #include <sys/socket.h>
 
-// Bibliotecas para uso de barramento CAN por dispositivo Linux
+// Bibliotecas para uso de barramento CAN por dispositivo com sistema operacional Linux
 #include <linux/can.h>
 #include <linux/can/raw.h>
 
-#define n_pernas 6 // Número de pernas do robô formiga
-#define n_char 2 // Número de caracteres da mensagem CAN. Cada caractere possui 8 bits
-#define CAN_send 1 // Variável de controle para habilitar o envio de mensagens por barramento CAN
-#define periodo_robo 2 // Período de movimento completo do robô em segundos
+#define n_pernas 6 // Número de pernas do robô-formiga
 
-// Define variáveis globais de socket CAN caso a variável de controle p/ envio de mensagens por barramento CAN seja definida
+/* Número de caracteres da mensagem de controle das pernas robóticas a ser enviada por CAN.
+Cada caractere possui 8 bits, sendo necessários, no mínimo, (6 pernas)*(2 bits/perna) = 12 bits
+para controlar o deslocamento das pernas do robô-formiga. Sendo assim, serão utilizados 2 caracteres para
+transmissão da mensagem. */
+#define n_char 2
+
+#define CAN_send 1 // Variável de controle para habilitar o envio de mensagens por barramento CAN
+#define periodo_robo 5 // Tempo de movimento do robô antes de parar, em segundos
+
+// Variáveis globais de criação de socket para comunicação via CAN
 #ifdef CAN_send
 	int s;
 	struct sockaddr_can addr;
@@ -29,7 +35,7 @@
 
 /*----------------------------------------------------------------------------------------*/
 
-void mensagem_perna(int msg[n_char])
+void mensagem_CAN(int msg[n_char])
 	
 	#ifdef CAN_send	
 		sprintf(frame.data, msg); // Envio da mensagem
@@ -54,82 +60,86 @@ void mensagem_perna(int msg[n_char])
 
 /*----------------------------------------------------------------------------------------*/
 
-void envia_movimento(int deslocamento)
+void executa_movimento(int deslocamento, int K)
 {
-	int msg[n_char];
-	int bit; // Posição do primeiro bit da dupla de variáveis para cada perna
-	int byte; // Número do caractere CAN com as configurações das pernas
+	int msg[n_char] = {0};
+	int bit; // Posição do bit da dupla de variáveis para cada perna
+	int byte = 0; // Número do caractere CAN com as configurações das pernas
 	
-	for(unsigned int i = 0; i < n_char; i++)
+	while(byte < n_char)
 	{
-		msg[i] = 0;
-	}
-	
-	for (unsigned int perna = 1; perna <= n_pernas; perna++)
-	{		
-		// bit = 0, 2, 4...
-		if(perna <= 4)
+		for (unsigned int perna = 1; perna <= n_pernas + 1; perna++)
 		{
-			bit = 2*(perna - 1);
-			byte = 0;
-		}
-		else
-		{
-			bit = 2*(perna - 5);
-			byte = 1;
-		}
-		
-		if(deslocamento == 0)
-		{
-			msg[byte] = msg[byte] & !(0xA0 >> bit);
-			printf("\nRegistrando 0 no bit %d do byte %d. Mensagem: %d", bit, byte, msg[byte]); // Perna vai para trás
-		}
-		else
-		{
-			msg[byte] = msg[byte] | (0xA0 >> bit);
-			printf("\nRegistrando 1 no bit %d do byte %d. Mensagem: %d", bit, byte, msg[byte]); // Perna vai para frente
+			if(perna > 4)
+			{
+				byte++;
+			}				
+			else if(perna > n_pernas)
+			{
+				byte++;
+				break;
+			}
+			
+			if(perna <= 4)
+			{
+				bit = 2*(perna - 1); // bit = 0, 2, 4, 6
+			}
+			else
+			{
+				bit = 2*(perna - 5); // bit = 0, 2
+			}
+			
+			if(perna % 2 == 0 && !K) // Se número da perna for par e flag K for false, perna levanta
+			{
+				msg[byte] = msg[byte] | (0xA0 >> bit);
+				printf("\nRegistrando 1 no bit %d do byte %d. Mensagem: %b\n", bit, byte, msg[byte]);
+			}
+			else if(perna % 2 == 0 && K) // Se número da perna for par e flag K for true, perna abaixa
+			{
+				msg[byte] = msg[byte] & !(0xA0 >> bit);
+				printf("\nRegistrando 0 no bit %d do byte %d. Mensagem: %b\n", bit, byte, msg[byte]);
+			}
+			else if(perna % 2 != 0 && !K) // Se número da perna for ímpar e flag K for false, perna abaixa
+			{
+				msg[byte] = msg[byte] & !(0xA0 >> bit);
+				printf("\nRegistrando 0 no bit %d do byte %d. Mensagem: %b\n", bit, byte, msg[byte]);
+			}
+			else // Se número da perna for ímpar e flag K for true, perna levanta
+			{
+				msg[byte] = msg[byte] | (0xA0 >> bit);
+				printf("\nRegistrando 1 no bit %d do byte %d. Mensagem: %b\n", bit, byte, msg[byte]);
+			}
+			
+			bit++; // bit = 1, 3, 5, 7
+			
+			if(deslocamento == 0)
+			{
+				msg[byte] = msg[byte] & !(0xA0 >> bit); // Operação AND bit-a-bit
+				printf("\nRegistrando 0 no bit %d do byte %d. Mensagem: %b\n", bit, byte, msg[byte]); // Robô vai para trás
+			}
+			else
+			{
+				msg[byte] = msg[byte] | (0xA0 >> bit); // Operação OR bit-a-bit
+				printf("\nRegistrando 1 no bit %d do byte %d. Mensagem: %b\n", bit, byte, msg[byte]); // Robô vai para frente
+			}			
 		}		
-		
-		bit++; // bit = 1, 3, 5...		
-		
-		if(perna % 2 == 0) // Se número da perna for par, perna levanta
-		{
-			msg[byte] = msg[byte] | (0xA0 >> bit);
-			printf("\nRegistrando 1 no bit %d do byte %d. Mensagem: %d", bit, byte, msg[byte]);
-		}
-		else // Se número da perna for ímpar, perna abaixa
-		{
-			msg[byte] = msg[byte] & !(0xA0 >> bit);
-			printf("\nRegistrando 0 no bit %d do byte %d. Mensagem: %d", bit, byte, msg[byte]);
-		}
-				
-	}
-	mensagem_perna(msg);
+	}	
+	mensagem_CAN(msg);	
 }
-
-/*----------------------------------------------------------------------------------------*/
-
-// Diferentes estados do módulo Toradex
-enum estados{enviar,aguardar,sair}estado;
 
 /*----------------------------------------------------------------------------------------*/
 
 int main(int argc, char **argv) // Programa principal
 {
-	char opcao[32];	
-	estado = enviar;
-	clock_t t_wait = clock(); // Tempo inicial
-	
-	/* Variável para indicar se o movimento do robô deve continuar ou parar.
-	Se parar = 0, robô deve continuar movimento.
-	Se parar = 1, robô deve parar.*/
-	int parar = 0;
+	char opcao[7];
+	clock_t t0 = clock(); // Tempo inicial
+	int k = 0; // Flag de controle das pernas
 	
 	/*----------- Abrindo socket CAN -------------*/
 	#ifdef CAN_send		
 		if ((s = socket(PF_CAN, SOCK_RAW, CAN_RAW)) < 0)
 		{
-			perror("Socket opening failed");
+			perror("Socket failed");
 			return 1;
 		}
 		
@@ -150,64 +160,51 @@ int main(int argc, char **argv) // Programa principal
 	#endif
 	/*-----------------------------------------------*/
 
-	// Máquina de estados do robô em loop
+	// Operação do robô em loop
 	while(1)
-	{
-		switch(estado)
+	{		
+		printf("\nInsira comando de deslocamento para o robo (frente/tras/sair): ");
+		scanf("%s", opcao);
+		
+		switch(opcao)
 		{
-			case enviar:
-				printf("\nInsira comando de deslocamento para o robo (frente/tras/sair)\n");
-				scanf( "%s", opcao);
-				
-				if((strcmp(opcao, "frente")) == 0)
-				{
-					envia_movimento(1);
-					estado = aguardar;
-					t_wait = clock();
-					parar = 0;
-					printf("\nAndando para frente\n");
-				}
-				else if((strcmp(opcao, "tras")) == 0)
-				{
-					envia_movimento(0);
-					estado = aguardar;
-					t_wait = clock();
-					parar = 0;
-					printf("\nAndando para tras\n");
-				}
-				else if((strcmp(opcao, "sair")) == 0)
-				{
-					estado = sair;
-				}
-				else 
-				{
-					printf("\nComando invalido\n");
-				}				
-				break;
-				
-			// Neste instante, o clock() marca um tempo posterior a t_wait
-			case aguardar:
-				if( ( (clock() - t_wait) / CLOCKS_PER_SEC ) > periodo_robo )
-				{
-					parar = 1;
-					estado = enviar;
-				}		
-				break;
-
-			case sair:
-				exit(0); // Encerra programa
-				break;
+			case "frente":
+			printf("\nMovimento para frente\n");
+			while( ( clock() - t0 ) / CLOCKS_PER_SEC <= periodo_robo )
+			{
+				executa_movimento(1, k);
+				k = !k;
+			}			
+			t0 = clock();
+			break;
+			
+			case "tras":
+			printf("\nMovimento para tras\n");
+			while( ( clock() - t0 ) / CLOCKS_PER_SEC <= periodo_robo )
+			{
+				executa_movimento(0, k);
+				k = !k;
+			}			
+			t0 = clock();
+			break;
+			
+			case "sair":
+			printf("\nPrograma encerrado\n");
+			exit(0); // Encerra programa
+			break;
+			
+			default:
+			printf("\nComando invalido\n");
 		}
 	}
 		
 	#ifdef CAN_send
 		if(close(s) < 0)
 		{
-			perror("Close failed");
+			perror("Socket closing failed");
 			return 1;
 		}
 	#endif
 
-	printf("\nFim do programa\n");
 	return 0;
 }
