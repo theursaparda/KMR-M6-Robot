ad05a91840d930f24c0d55187f406fdf4ab6cf48
madfelps <felipemoura@usp.br>
2021-12-24 00:08:03 -0300

    Movimentos estruturados

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// #include "mbed.h"
+// #include "Servo.h"
+
+Servo servo_vertical_legA(p21);
+Servo servo_horizontal_legA(p22);
+Servo servo_vertical_legB(p23);
+Servo servo_horizontal_legB(p24);
+Servo servo_vertical_legC(p25);
+Servo servo_horizontal_legC(p26);
+
+
 void (*FunctionPointer)();
 
 void State_Initial();
@@ -8,6 +19,7 @@ void State_Forward();
 void State_Backward();
 void State_RotateRight();
 void State_RotateLeft();
+void State_Null();
 
 void State_Initial()
 {
@@ -18,28 +30,95 @@ void State_Initial()
 void State_Forward()
 {
   printf("Andar para frente\n");
-  FunctionPointer = State_Backward;
+
+  for(float i = 0; i < 1; i+=0.05)
+  {
+    servo_vertical_legA     = i;
+    servo_vertical_legB     = i;
+    servo_vertical_legC     = i;
+    wait(0.1);
+    servo_horizontal_legA   = i;
+    servo_horizontal_legB   = i;
+    servo_horizontal_legC   = i;
+    wait(0.1);
+
+  }
+
 }
 
 void State_Backward()
 {
-  printf("Andar para tras\n");
-  FunctionPointer = State_RotateRight;
+  for(float i = 1; i < 1; i-=0.05)
+  {
+    servo_vertical_legA     = i;
+    servo_vertical_legB     = i;
+    servo_vertical_legC     = i;
+    wait(0.1);
+    servo_horizontal_legA   = i;
+    servo_horizontal_legB   = i;
+    servo_horizontal_legC   = i;
+    wait(0.1);
+  }
+
 }
 
 void State_RotateRight(){
   printf("Girar para direita\n");
-  FunctionPointer = State_RotateLeft;
+
+  for(float i = 0; i < 1; i+=0.05)
+  {
+    servo_vertical_legA     = i;
+    servo_vertical_legB     = i;
+    servo_vertical_legC     = i;
+    wait(0.1);
+    servo_horizontal_legA   = 1-i;
+    servo_horizontal_legB   = 1-i;
+    servo_horizontal_legC   = 1-i;
+    wait(0.1);
+
+  }
+
+
+  
 }
 
 void State_RotateLeft()
 {
   printf("Girar para esquerda\n");
-  FunctionPointer = State_Initial;
+
+  for(float i = 0; i < 1; i+=0.05)
+  {
+    servo_vertical_legA     = 1-i;
+    servo_vertical_legB     = 1-i;
+    servo_vertical_legC     = 1-i;
+    wait(0.1);
+    servo_horizontal_legA   = i;
+    servo_horizontal_legB   = i;
+    servo_horizontal_legC   = i;
+    wait(0.1);
+
+  }
+
+}
+
+void State_Null()
+{
+
 }
 
 
 int main(void) {
+
+  servo.calibrate();
+
+  servo_vertical_legA   = 0;
+  servo_vertical_legB   = 0;
+  servo_vertical_legC   = 0;
+  servo_horizontal_legA = 0;
+  servo_horizontal_legB = 0;
+  servo_horizontal_legC = 0;
+
+
   FunctionPointer = State_Initial;
 
   while(1)
